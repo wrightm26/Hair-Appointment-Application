773557bb6227fa8435f1eb3de9b32810c00bfaf6
Mickel <mswright5981@gmail.com>
2023-05-08 12:31:50 -0400

    home page and added review and welcome note

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
