a9f065146eebec8a6e2bc465acac299c2159edd1
Mickel <mswright5981@gmail.com>
2023-05-11 14:43:37 -0400

    added sign in page and form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.form import SignUpForm
 
 @app.route('/')
 def index():
@@ -8,6 +9,19 @@ def index():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        full_name = form.full_name.data
+        usernames = form.username.data
+        passwords = form.password.data
+        numbers = form.password.data
+        email = form.email.data
+        return render_template('login.html')
+
+    return render_template('signup.html', form=form)
 
 @app.route('/locservices')
 def locservices():
@@ -18,5 +32,25 @@ def braidservices():
     return render_template('braidservices.html')
 
 @app.route('/gallery')
-def braidservices():
+def gallery():
     return render_template('gallery.html')
+
+# @app.route('/order/<product_id>/<user_id>', methods=["GET", "POST"])
+# @login_required
+# def order(product_id, user_id):
+
+#     pull_art = stripe.Product.retrieve(product_id)
+#     price_id = pull_art.get("default_price")
+
+#     session = stripe.checkout.Session.create(
+#         line_items = [{
+#             'price': price_id,
+#             'quantity': 1,
+#         }],
+#         mode = 'payment',
+#         success_url = 'http://localhost:5000' + url_for('thankyou', product_id=product_id, user_id=user_id),
+#         cancel_url = 'http://localhost:5000' + url_for('cancel')
+
+#     )
+
+#     return redirect(session.url, code=303)
